01729382d7c2013106cb849b3ff34f51911271e2
awe19001 <joeverlyncapute@gmail.com>
2025-05-06 07:38:17 +0800

    mission statement Pary 2

--- WDD131/mission/mission.js
+++ WDD131/mission/mission.js
@@ -8,17 +8,21 @@ themeSelector.addEventListener("change", changeTheme);
 // Define the function to change themes
 function changeTheme() {
     const selectedTheme = themeSelector.value;
+    console.log("Selected theme:", selectedTheme); // Debugging line
+
   
     if (selectedTheme === "dark") {
     // Add the dark class to the body
       document.body.classList.add("dark");
     // Change the logo to the white version
-      logo.setAttribute("src", "byui-logo_white.webp"); 
+      logo.setAttribute("src", "./byui-logo_white.png");
+      console.log("Logo changed to white."); // Debugging line
     } else {
     // Remove the dark class    
       document.body.classList.remove("dark");
     // Change the logo to the blue version  
-      logo.setAttribute("src", "byui-logo_blue.webp"); 
+      logo.setAttribute("src", "./byui-logo_blue.webp"); 
+      console.log("Logo changed to blue."); // Debugging line
     }
   }
 
