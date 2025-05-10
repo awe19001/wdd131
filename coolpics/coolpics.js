cc0d63fa504e6cc5e3fac1f2cdf72cc0d7419fb2
awe19001 <joeverlyncapute@gmail.com>
2025-05-11 06:27:35 +0800

    cool pics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,13 +1,3 @@
-const thumbnail = document.getElementById("thumbnail");
-const overlay = document.getElementById("overlay");
-
-thumbnail.addEventListener("click", () => {
-  overlay.classList.remove("hidden");
-});
-
-overlay.addEventListener("click", () => {
-  overlay.classList.add("hidden");
-});
 
 
 function toggleMenu() {
