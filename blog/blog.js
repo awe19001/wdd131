32f8a715d7218ec5b99010fa10f2d578557ac98b
awe19001 <joeverlyncapute@gmail.com>
2025-05-24 05:11:39 +0800

    at last at least got  95%

--- blog/blog.js
+++ blog/blog.js
@@ -5,7 +5,7 @@ const articles = [
     date: 'July 5, 2022',
     description: 'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
     imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
-    imgAlt: 'Book cover for Septimus Heap 1',
+    imgAlt:  'Cover art of "Septimus Heap Book One: Magyk" by Angie Sage',
     ages: '10-14',
     genre: 'Fantasy',
     stars: '****'
@@ -16,7 +16,7 @@ const articles = [
     date: 'December 12, 2021',
     description: 'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
     imgSrc: 'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
-    imgAlt: 'Book cover for Magnus Chase 1',
+    imgAlt: 'Illustrated cover of "Magnus Chase: Sword of Summer" by Rick Riordan',
     ages: '12-16',
     genre: 'Fantasy',
     stars: '⭐⭐⭐⭐'
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <p><strong>Rating:</strong> ${article.stars}</p>
       </div>
       <div class="content">
-        <h3>${article.title}</h3>
+        <h2>${article.title}</h2>
         <img src="${article.imgSrc}" alt="${article.imgAlt}">
         <p>${article.description}</p>
       </div>
