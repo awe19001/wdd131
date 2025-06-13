a56aac57719b5559ab353c30ce18c9cb15ee5325
awe19001 <joeverlyncapute@gmail.com>
2025-06-13 09:34:10 +0800

    ponder8 ES modules

---
+++ ponder8/Course.mjs
@@ -0,0 +1,63 @@
+// Course.mjs
+const aCourse = {
+  code: "CSE121b",
+  name: "Javascript Language",
+  sections: [
+    {
+      sectionNum: 1,
+      roomNum: "STC 353",
+      enrolled: 26,
+      days: "TTh",
+      instructor: "Bro T",
+    },
+    {
+      sectionNum: 2,
+      roomNum: "STC 347",
+      enrolled: 25,
+      days: "TTh",
+      instructor: "Sis A",
+    },
+  ],
+  // Initialize display on page load
+  init: function () {
+    setCourseInfo(this);
+    renderSections(this.sections);
+  },
+
+  changeEnrollment: function (sectionNum, add = true) {
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNum == sectionNum
+    );
+    if (sectionIndex >= 0) {
+      if (add) {
+        this.sections[sectionIndex].enrolled++;
+      } else {
+        this.sections[sectionIndex].enrolled--;
+      }
+      renderSections(this.sections);
+    }
+  },
+};
+
+// Function to display course info on the page
+function setCourseInfo(course) {
+  const courseName = document.querySelector("#courseName");
+  const coursecode = document.querySelector("#courseCode");
+  courseName.textContent = course.name;
+  coursecode.textContent = course.code;
+}
+
+// Function to render the list of sections in the table body
+function renderSections(sections) {
+  const html = sections.map(
+    (section) => `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+  );
+  document.querySelector("#sections").innerHTML = html.join("");
+}
+
+export default aCourse;
