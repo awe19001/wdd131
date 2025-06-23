2b4b2ace36fbc1cd5af826b172044fa80aa94ba7
awe19001 <joeverlyncapute@gmail.com>
2025-06-23 08:57:22 +0800

    recipe 2 ... on the process of making

--- recipes copy/recipes.mjs
+++ recipes copy/recipes.mjs
@@ -1,17 +1,26 @@
 export const recipes = [
   {
-    title: 'Spaghetti Bolognese',
-    description: 'A classic Italian pasta dish made with rich meat sauce.',
-    image: 'images/recipe1.jpg',
+    title: 'Black Beans and Rice',
+    description: 'A classic Black Beans and Rice recipe blends tender black beans with aromatic spices, sautéed vegetables, and perfectly cooked rice. ',
+    image: 'images/images/black-beans-and-rice.jpg',
+    tags: ['Black Beans', 'Beans and Rice'],
+    rating: 4,
+    category: 'Dinner'
   },
   {
     title: 'Chicken Curry',
     description: 'A spicy and flavorful dish with tender chicken pieces.',
-    image: 'images/recipe2.jpg',
+    image: 'images/images/chicken-curry.webp',
+    tags: ['Indian', 'Spicy'],
+    rating: 5,
+    category: 'Lunch'
   },
   {
-    title: 'Vegetarian Tacos',
-    description: 'Fresh and healthy tacos with beans and veggies.',
-    image: 'images/recipe3.jpg',
+    title: 'Chocolate Chip Cookies',
+    description: 'Soft in the center with golden crispy edges, these classic chocolate chip cookies are the ultimate comfort treat.',
+    image: 'images/images/chocolate-chip-cookies.jpg',
+    tags: ['Dessert', 'Cookies'],
+    rating: 3,
+    category: 'Snack'
   },
 ];
