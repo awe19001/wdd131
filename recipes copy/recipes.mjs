2a538c52a36867a638f27fbf809ecd73df16b264
awe19001 <joeverlyncapute@gmail.com>
2025-06-23 18:40:34 +0800

    add recipe in mjs

--- recipes copy/recipes.mjs
+++ recipes copy/recipes.mjs
@@ -3,7 +3,6 @@ export const recipes = [
     title: 'Black Beans and Rice',
     description: 'A classic Black Beans and Rice recipe blends tender black beans with aromatic spices, sautéed vegetables, and perfectly cooked rice. ',
     image: 'images/images/black-beans-and-rice.jpg',
-    tags: ['Black Beans', 'Beans and Rice'],
     rating: 4,
     category: 'Dinner'
   },
@@ -11,7 +10,6 @@ export const recipes = [
     title: 'Chicken Curry',
     description: 'A spicy and flavorful dish with tender chicken pieces.',
     image: 'images/images/chicken-curry.webp',
-    tags: ['Indian', 'Spicy'],
     rating: 5,
     category: 'Lunch'
   },
@@ -19,8 +17,35 @@ export const recipes = [
     title: 'Chocolate Chip Cookies',
     description: 'Soft in the center with golden crispy edges, these classic chocolate chip cookies are the ultimate comfort treat.',
     image: 'images/images/chocolate-chip-cookies.jpg',
-    tags: ['Dessert', 'Cookies'],
     rating: 3,
+    category: 'Snack'
+  },
+   {
+    title: 'Apple Crisp',
+    description: 'A dessert made with tender baked apples, fragrant cinnamon, and a buttery oat topping that bakes to golden perfection.',
+    image: 'images/images/apple-crisp.jpg',
+    rating: 4,
+    category: 'Dessert'
+  },
+   {
+    title: 'Escalopes de Poulet à la Crème',
+    description: 'A French favorite, this dish features tender chicken cutlets sautéed until golden, then simmered in a luxurious cream sauce infused with white wine, shallots, and fresh herbs.',
+    image: 'images/images/escalopes-de-poulet-a-la-creme.webp',
+    rating: 4,
+    category: 'Dinner'
+  },
+   {
+    title: 'German Gooseberry Cake (Stachelbeerkuchen)',
+    description: 'This traditional German cake combines a soft, buttery sponge with tart gooseberries and a sweet streusel topping.',
+    image: 'images/images/german-gooseberry-cake.jpg',
+    rating: 5,
+    category: 'Snack'
+  },
+  {
+    title: 'Roasted Potatoes',
+    description: 'Crispy on the outside and fluffy on the inside, these golden roasted potatoes are seasoned to perfection with herbs, garlic, and olive oil.',
+    image: 'images/images/roasted-potatoes.webp',
+    rating: 5,
     category: 'Snack'
   },
 ];
